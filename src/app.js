6fac6352b2bfba64976520be8bef840ecf74e46c
JosephSB <jguku2002@gmail.com>
2022-03-13 13:31:50 -0500

    new params

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const cors = require("cors");
 const venom = require('venom-bot');
 const {isConnected} = require("./middlewares/VenomBot.js");
+const chromiumArgs = require("./config/chromiunArgs.js");
 
 global.isConnectedToVenom = false;
 venom
@@ -38,42 +39,7 @@ venom
         { 
             logQR: false,
             autoClose: 0,
-            browserArgs: [
-                '--no-zygote',
-                '--log-level=3',
-                '--disable-site-isolation-trials',
-                '--no-experiments',
-                '--ignore-gpu-blacklist',
-                '--ignore-certificate-errors',
-                '--ignore-certificate-errors-spki-list',
-                '--disable-gpu',
-                '--disable-extensions',
-                '--disable-default-apps',
-                '--enable-features=NetworkService',
-                '--disable-setuid-sandbox',
-                '--no-sandbox',
-                '--disable-webgl',
-                '--disable-infobars',
-                '--window-position=0,0',
-                '--ignore-certifcate-errors',
-                '--ignore-certifcate-errors-spki-list',
-                '--disable-threaded-animation',
-                '--disable-threaded-scrolling',
-                '--disable-in-process-stack-traces',
-                '--disable-histogram-customizer',
-                '--disable-gl-extensions',
-                '--disable-composited-antialiasing',
-                '--disable-canvas-aa',
-                '--disable-3d-apis',
-                '--disable-accelerated-2d-canvas',
-                '--disable-accelerated-jpeg-decoding',
-                '--disable-accelerated-mjpeg-decode',
-                '--disable-app-list-dismiss-on-blur',
-                '--disable-accelerated-video-decode',
-                '--disable-dev-shm-usage',
-                '--disable-gl-drawing-for-tests',
-                '--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
-            ],
+            browserArgs: chromiumArgs,
         }
     )
     .then((client) => {
